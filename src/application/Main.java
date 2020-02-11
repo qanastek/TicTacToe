9ca0d07c3c1f1adf756e66cab244e3bc908878b3
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2020-02-11 15:42:48 +0100

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+			primaryStage.setTitle("Projet GUI V1.0");
+
+//			BorderPane root = new BorderPane();
+
+		    Parent layout = FXMLLoader.load(getClass().getResource("vues/Main.fxml"));
+			
+			Scene scene = new Scene(layout,400,400);
+			scene.getStylesheets().add(getClass().getResource("vues/application.css").toExternalForm());
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
